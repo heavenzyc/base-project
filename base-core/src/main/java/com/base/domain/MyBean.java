c6e07abf10cac748dddca7ef5c00129a452105df
heaven.zyc <heavenzyc@live.cn>
2014-08-13 18:34:45 +0800

    submit

---
+++ base-core/src/main/java/com/base/domain/MyBean.java
@@ -0,0 +1,7 @@
+package com.base.domain;
+
+/**
+ * Created by heaven.zyc on 14-8-13.
+ */
+public class MyBean {
+}
---
+++ base-web/src/main/java/com/base/controller/MyBean.java
@@ -0,0 +1,7 @@
+package com.base.controller;
+
+/**
+ * Created by heaven.zyc on 14-8-13.
+ */
+public class MyBean {
+}
