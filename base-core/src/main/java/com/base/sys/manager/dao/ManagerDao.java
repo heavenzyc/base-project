2a39806d2220bfe971ba7b87cfd122abf3525a97
heavenzyc <heavenzyc@live.cn>
2014-10-21 23:56:23 +0800

    submit

--- base-core/src/main/java/com/base/sys/manager/dao/ManagerDao.java
+++ base-core/src/main/java/com/base/sys/manager/dao/ManagerDao.java
@@ -14,26 +14,4 @@ import java.util.List;
  */
 @Repository
 public class ManagerDao extends BaseDaoImpl<Manager,Integer> {
-
-    public List<Manager> getAll(){
-        Session s = getHibernateTemplate().getSessionFactory().getCurrentSession();
-        Criteria c = s.createCriteria(Manager.class);
-        c.setCacheable(true);//这句必须要有
-        System.out.println("第一次读取");
-        s.beginTransaction();
-        List<Manager> users = c.list();
-        s.getTransaction().commit();
-        System.out.println(users.size());
-
-        s = getHibernateTemplate().getSessionFactory().getCurrentSession();
-        c = s.createCriteria(Manager.class);
-        c.setCacheable(true);//这句必须要有
-        System.out.println("第二次读取");
-        s.beginTransaction();
-        users = c.list();
-        s.getTransaction().commit();
-        System.out.println(users.size());
-        s.close();
-        return null;
-    }
 }
