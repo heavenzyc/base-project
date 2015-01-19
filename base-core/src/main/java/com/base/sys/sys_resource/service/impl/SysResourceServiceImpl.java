fd7d847394491d6689e3425aaff6f24aedf8f15f
heavenzyc <heaven880210>
2015-01-19 18:21:09 +0800

    submit

--- base-core/src/main/java/com/base/sys/sys_resource/service/impl/SysResourceServiceImpl.java
+++ base-core/src/main/java/com/base/sys/sys_resource/service/impl/SysResourceServiceImpl.java
@@ -1,9 +1,24 @@
 package com.base.sys.sys_resource.service.impl;
 
+import com.base.sys.sys_resource.dao.SysResourceDao;
+import com.base.sys.sys_resource.model.SysResource;
 import com.base.sys.sys_resource.service.SysResourceService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by zkdu on 2015/1/9.
  */
+@Service("sysResourceService")
 public class SysResourceServiceImpl implements SysResourceService {
+    @Resource
+    private SysResourceDao sysResourceDao;
+
+    @Override
+    public List<SysResource> getAll() {
+        List<SysResource> list = sysResourceDao.findAll();
+        return list;
+    }
 }
