5c23fc2f06c90cb470c72ee6f47f6c8f708dd0b8
heavenzyc <heaven880210>
2015-01-20 18:05:12 +0800

    submit

--- base-core/src/main/java/com/base/sys/sys_resource/service/impl/SysResourceServiceImpl.java
+++ base-core/src/main/java/com/base/sys/sys_resource/service/impl/SysResourceServiceImpl.java
@@ -6,6 +6,7 @@ import com.base.sys.sys_resource.service.SysResourceService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,5 +21,27 @@ public class SysResourceServiceImpl implements SysResourceService {
     public List<SysResource> getAll() {
         List<SysResource> list = sysResourceDao.findAll();
         return list;
+    }
+
+    @Override
+    public List<SysResource> getMenus() {
+        List<SysResource> first = sysResourceDao.find(" from SysResource where type=1 ");
+        this.test(first,1);
+        return first;
+    }
+
+    private void test(List<SysResource> resources, Integer type) {
+        type++;
+        List<SysResource> result = new ArrayList<SysResource>();
+        if (type > 3 || resources.size() == 0) return;
+        String hql = " from SysResource where id like ? and type=? ";
+        for (SysResource parent : resources) {
+            List<SysResource> list = sysResourceDao.find(hql,parent.getId()+"%",parent.getType()+1);
+            parent.setSubRes(list);
+            for (SysResource child : list) {
+                result.add(child);
+            }
+        }
+        test(result,type);
     }
 }
